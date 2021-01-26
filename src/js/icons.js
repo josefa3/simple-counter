c6ff535175767195a2eba1cd3c4173c5047066e6
josefa3 <sefaherrera391993@gmail.com>
2021-01-25 23:13:00 -0400

    falta agregar boton para reiniciar

--- src/js/icons.js
+++
@@ -1,6 +0,0 @@
-import fontawesome from `@fortawesome/fonstawesome`;
-import faClock from `@fortawesome/fonstawesome-free-regular/faClock`;
-fontawesome.config = {
-    autoReplaceSvg: `nest`
-}
-fontawesome.library.add(faClock);
--- src/js/index.js
+++ src/js/index.js
@@ -1,29 +1,75 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
+import PropTypes from "prop-types";
 
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
 //include your index.scss file into the bundle
 import "../styles/index.scss";
-import "icons.js";
 
-const Contador = props => {
+window.on = true;
+function Contador(props) {
 	return (
 		<div className="contadorTotal">
 			<div className="relojIcon">
-				<i class="far fa-clock" />
+				<i className="far fa-clock" />
 			</div>
-			<div className="seis">0</div>
-			<div className="cinco">0</div>
-			<div className="cuatro">0</div>
-			<div className="tres">0</div>
-			<div className="dos">0</div>
-			<div className="uno">0</div>
+			<div className="hora">
+				{props.hora.toString().length > 1
+					? props.hora
+					: `0${props.hora}`}
+			</div>
+			<div className="minuto">
+				{props.minuto.toString().length > 1
+					? props.minuto
+					: `0${props.minuto}`}
+			</div>
+			<div className="segundo">
+				{props.segundo.toString().length > 1
+					? props.segundo
+					: `0${props.segundo}`}
+			</div>
+			<button
+				type="button"
+				className="btn btn-primary"
+				onClick={() => {
+					window.on = !window.on;
+				}}>
+				{props.buttonLabelPararRei}
+			</button>
 		</div>
 	);
+}
+Contador.propTypes = {
+	hora: PropTypes.number,
+	minuto: PropTypes.number,
+	segundo: PropTypes.number,
+	buttonLabelPararRei: PropTypes.string
+	// buttonLabelComenzar: PropTypes.string
 };
-
 //render your react application
-ReactDOM.render(<Contador />, document.querySelector("#app"));
+let timer = 0;
+setInterval(function() {
+	let segundos = 0;
+	let minutos = 0;
+	let horas = 0;
+	let total = timer;
+	horas = Math.floor(total / 3600);
+	total = total % 3600;
+	minutos = Math.floor(total / 60);
+	segundos = total % 60;
+	window.on && timer++;
+
+	ReactDOM.render(
+		<Contador
+			segundo={segundos}
+			minuto={minutos}
+			hora={horas}
+			buttonLabelPararRei="Parar/ Iniciar"
+			// buttonLabelComenzar="Reiniciar"
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
